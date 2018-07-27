e80cee17910c81d7d96df7d2ac68791c4ba5a68b
Andrew Stakhov <andrew.stakhov@incryptex.com>
2018-07-26 20:00:32 -0400

    Initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using SharpCifs.Smb;
+
+namespace ShareMount.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET
+        public IActionResult Index()
+        {
+            return
+            View(new FormModel() {Url="smb://UserName:Password@ServerIP/ShareName/FolderName/"});
+        }
+
+        [HttpPost]
+        public IActionResult List(FormModel model)
+        {
+            try
+            {
+                var folder = new SmbFile(model.Url);
+                model.Files = folder.ListFiles().Select(x => x.GetName()).ToList();
+            }
+            catch (Exception e)
+            {
+                model.Exception = e.ToString();
+            }
+            
+            return View("Index", model);
+        }
+        
+    }
+}
---
+++ FormModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using SharpCifs.Smb;
+
+namespace ShareMount
+{
+    public class FormModel
+    {
+        public string Url { get; set; }
+        public List<string> Files { get; set; }
+        public string Exception { get; set; }
+    }
+}
